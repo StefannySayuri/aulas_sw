c06404e82051eca2e9f50604ef5b5389a78f39aa
Sayuri <125521143+StefannySayuri@users.noreply.github.com>
2024-05-17 17:36:29 -0300

    Add files via upload

---
+++ code.js
@@ -0,0 +1,15 @@
+function errado(){
+    alert("Incorreto!")
+}
+
+function certo(){
+    alert("Correto!")
+}
+
+function numero(x){
+    if (x%2==0) {
+        alert("Este número é par!")
+    } else {
+        alert("Este número é ímpar!")
+    }
+}
